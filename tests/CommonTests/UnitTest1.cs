eafca649f685da03f8b444b6e71b4d9c1b3e03a1
lienpiao <511169817@qq.com>
2020-02-18 22:23:35 +0800

    Common-xUnit单元测试

---
+++ tests/CommonTests/UnitTest1.cs
@@ -0,0 +1,22 @@
+using System;
+using Xunit;
+
+namespace CommonTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            /*
+             * 测试用例需要打上[Fact]特性标记，Fact特性还可以给测试用例取别名
+             * Trait特性可以对测试用例进行分组说明
+             * 测试要分为三部分（AAA）：Arrange、Act、Assert：
+             * 1.安排对象，根据需要对其进行创建和设置
+             * 2.作用于对象
+             * 3.断言某些项按预期进行             
+             */
+
+        }
+    }
+}
