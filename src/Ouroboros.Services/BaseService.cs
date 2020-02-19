5e1d80e06c0e69d920cb900f4a869955300ff8cf
lienpiao <511169817@qq.com>
2020-02-19 18:18:12 +0800

    基类

---
+++ src/Ouroboros.Services/BaseService.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Ouroboros.Common.IDCode.Snowflake;
+using Ouroboros.DbContexts;
+using Ouroboros.UnitOfWork;
+using System;
+
+namespace Ouroboros.Services
+{
+    public interface IBaseService
+    {
+    }
+    public class BaseService : IBaseService
+    {
+        public readonly IUnitOfWork<MSDbContext> _unitOfWork;
+        public readonly IMapper _mapper;
+        public readonly IdWorker _idWorker;
+
+        public BaseService(IUnitOfWork<MSDbContext> unitOfWork, IMapper mapper, IdWorker idWorker)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+            _idWorker = idWorker;
+        }
+    }
+
+}
--- src/Ouroboros.Services/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Ouroboros.Services
-{
-    public class Class1
-    {
-    }
-}
