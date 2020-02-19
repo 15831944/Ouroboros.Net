a5a0cff42c11141473c2e1b2cf1bebf1116fee88
lienpiao <511169817@qq.com>
2020-02-19 12:58:58 +0800

    SiteSetting网站配置实体类

--- src/Ouroboros.WebCore/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Ouroboros.WebCore
-{
-    public class Class1
-    {
-    }
-}
---
+++ src/Ouroboros.WebCore/SiteSetting.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ouroboros.WebCore
+{
+    public class SiteSetting
+    {
+        /// <summary>
+        /// 雪花算法参数
+        /// </summary>
+        public long WorkerId { get; set; }
+
+        /// <summary>
+        /// 雪花算法参数
+        /// </summary>
+        public long DataCenterId { get; set; }
+
+        /// <summary>
+        /// 是用户锁定后，多久可以重新登录
+        /// </summary>
+        public int LoginFailedCountLimits { get; set; }
+
+        /// <summary>
+        /// 用户锁定后，多久可以重新登录
+        /// </summary>
+        public int LoginLockedTimeout { get; set; }
+
+    }
+}
