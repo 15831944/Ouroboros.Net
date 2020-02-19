1d59530a385d1a6f3f0b9d1a299ca860b618ee1c
lienpiao <511169817@qq.com>
2020-02-19 11:43:20 +0800

    添加注释

--- src/Ouroboros.Entities/Core/IEntity.cs
+++ src/Ouroboros.Entities/Core/IEntity.cs
@@ -16,11 +16,34 @@ namespace Ouroboros.Entities.Core
     /// </summary>
     public abstract class BaseEntity : IEntity
     {
+        /// <summary>
+        /// 主键
+        /// </summary>
         public long Id { get; set; }
+
+        /// <summary>
+        /// 状态码
+        /// </summary>
         public StatusCode StatusCode { get; set; }
+
+        /// <summary>
+        /// 创建者
+        /// </summary>
         public long? Creator { get; set; }
+
+        /// <summary>
+        /// 创建时间
+        /// </summary>
         public DateTime? CreateTime { get; set; }
+
+        /// <summary>
+        /// 修改者
+        /// </summary>
         public long? Modifier { get; set; }
+
+        /// <summary>
+        /// 修改时间
+        /// </summary>
         public DateTime? ModifyTime { get; set; }
     }
 
