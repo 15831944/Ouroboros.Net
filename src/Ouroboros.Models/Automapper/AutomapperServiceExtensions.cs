a30138cb56333fcb7902ac7b6497a3f1710697ec
lienpiao <511169817@qq.com>
2020-02-19 18:08:45 +0800

    角色映射配置

---
+++ src/Ouroboros.Models/Automapper/AutomapperServiceExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ouroboros.Models.Automapper
+{
+    class AutomapperServiceExtensions
+    {
+    }
+}
---
+++ src/Ouroboros.Models/Automapper/SysRoleProfile.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using Ouroboros.Entities;
+using Ouroboros.Models.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ouroboros.Models.Automapper
+{
+    public class SysRoleProfile : Profile
+    {
+
+        public SysRoleProfile()
+        {
+
+            CreateMap<SysRoleViewModel, SysRole>();
+
+            //CreateMap<User, UserData>()
+            //    .ForMember(a => a.Id, t => t.MapFrom(b => b.Id))
+            //    .ForMember(a => a.RoleName, t => t.MapFrom(b => b.Role.Name))
+            //    .ForMember(a => a.RoleDisplayName, t => t.MapFrom(b => b.Role.DisplayName))
+            //    .ForMember(a => a.MainDepartmentId, t => t.MapFrom(b => b.UserDepartments.First(x => x.IsMainDepartment == true).Department.Id))
+            //    .ForMember(a => a.MainDepartmentDisplayName, t => t.MapFrom(b => b.UserDepartments.First(x => x.IsMainDepartment == true).Department.GetDisplayName()))
+            //    ;
+
+        }
+
+    }
+}
