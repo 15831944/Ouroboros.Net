d8e4029abe3f7f125aa95f4c35e4210754c9295d
lienpiao <511169817@qq.com>
2020-02-20 12:15:39 +0800

    获取业务层程序集名称

---
+++ src/Ouroboros.Services/ServiceExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Ouroboros.Services
+{
+    public static class ServiceExtensions
+    {
+        /// <summary>
+        /// 获取程序集名称
+        /// </summary>
+        /// <returns></returns>
+        public static string GetAssemblyName()
+        {
+            return Assembly.GetExecutingAssembly().GetName().Name;
+        }
+    }
+
+}
