44ca6d3cac07fe9362bbc0f48ac78aa124db2eee
lienpiao <511169817@qq.com>
2020-02-20 11:54:27 +0800

    替换DI容器

--- src/Ouroboros.WebApi/Initialize/Startup.cs
+++ src/Ouroboros.WebApi/Initialize/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Autofac;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -21,9 +22,33 @@ namespace Ouroboros.WebApi
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        /*        public Startup(IConfiguration configuration)
+                {
+                    Configuration = configuration;
+                }*/
+
+        public ILifetimeScope AutofacContainer { get; private set; }
+
+        public Startup(IWebHostEnvironment env)
         {
-            Configuration = configuration;
+            // In ASP.NET Core 3.0 `env` will be an IWebHostingEnvironment, not IHostingEnvironment.
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        /// <summary>
+        /// 添加autofac的DI配置容器
+        /// </summary>
+        /// <param name="builder"></param>
+        public void ConfigureContainer(ContainerBuilder builder)
+        {
+            //注册IBaseService和IRoleService接口及对应的实现类
+            builder.RegisterType<BaseService>().As<IBaseService>().InstancePerLifetimeScope();
+            builder.RegisterType<SysRoleService>().As<ISysRoleService>().InstancePerLifetimeScope();
         }
 
         public IConfiguration Configuration { get; }
@@ -38,17 +63,13 @@ namespace Ouroboros.WebApi
 
             //注册webcore服务（网站主要配置）
             services.AddWebCoreService(Configuration);
-            
+
             //注册工作单元
             services.AddUnitOfWorkService<MSDbContext>(options => { options.UseMySql(Configuration.GetSection("ConectionStrings:MSDbContext").Value); });
 
             //注册automapper服务
             services.AddAutomapperService();
 
-            //注册IBaseService和IRoleService接口及对应的实现类
-            services.AddScoped<IBaseService, BaseService>();
-            services.AddScoped<ISysRoleService, SysRoleService>();
-      
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
--- src/Ouroboros.WebApi/Program.cs
+++ src/Ouroboros.WebApi/Program.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -39,6 +40,7 @@ namespace Ouroboros.WebApi
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+            .UseServiceProviderFactory(new AutofacServiceProviderFactory())//Ìæ»»autofac×÷ÎªIDÈÝÆ÷
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
